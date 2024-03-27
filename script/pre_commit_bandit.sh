cbec8ce0a681e0dcfe38ea0fa33a85c7724120be
Brig Lamoreaux <briglx@users.noreply.github.com>
2024-03-27 19:54:39 +0000

    debug bandit

--- script/pre_commit_bandit.sh
+++ script/pre_commit_bandit.sh
@@ -10,11 +10,16 @@
 # Stop on errors
 set -e
 
-printf 'Running %s with args %s\n' "$0" "$*"
-
+echo "Running bandit pre-commit hook"
 which python
 which pip
+which bandit
+bandit --version
 pip freeze
+echo
+echo
+
+printf 'Running %s with args %s\n' "$0" "$*"
 
 for i do
     if ! bandit --quiet --format=custom --configfile pyproject.toml "$i"; then
