b1e2264b6c68546edf02b57b16b04c8376c9b981
Brig Lamoreaux <briglx@users.noreply.github.com>
2024-03-27 18:36:03 +0000

    pre-commit workaround for config

---
+++ script/pre_commit_bandit.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+###################################################################
+# Run bandit on passed in files.
+# Needed to ensure .config file is loaded
+#
+# Params
+#   Files. path to one or more json files separated by spaces
+###################################################################
+
+# Stop on errors
+set -e
+
+printf 'Running %s with args %s\n' "$0" "$*"
+for i do
+    if ! bandit --quiet --format=custom --configfile pyproject.toml "$i"; then
+        echo "bandit failed."
+        exit 1
+    fi
+done
